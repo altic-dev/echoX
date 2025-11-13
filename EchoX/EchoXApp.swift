4b6ddd73396e2b41537a05b1cae772a0d3e084ec
grohith327 <grohith327@gmail.com>
2025-11-12 22:16:14 -0800

    Initial Commit

---
+++ EchoX/EchoXApp.swift
@@ -0,0 +1,32 @@
+//
+//  EchoXApp.swift
+//  EchoX
+//
+//  Created by Barathwaj Anandan on 11/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EchoXApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
